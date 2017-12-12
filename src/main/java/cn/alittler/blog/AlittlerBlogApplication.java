dd0b38221bd93810538cd26f62a7a22846fc3481
Mr-LiuDC <442823529@qq.com>
2017-12-12 16:25:25 +0800

    initial project by spring starter tool

---
+++ src/main/java/cn/alittler/blog/AlittlerBlogApplication.java
@@ -0,0 +1,12 @@
+package cn.alittler.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlittlerBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlittlerBlogApplication.class, args);
+	}
+}
